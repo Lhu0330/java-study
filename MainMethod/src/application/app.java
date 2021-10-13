9245b4b28758249977ff38bc4291b33963dd9899
haun <gkdns12345@daum.net>
2021-10-13 11:17:24 +0900

    첫번째commit

---
+++ MainMethod/src/application/app.java
@@ -0,0 +1,15 @@
+package application;
+
+public class app {
+
+	public static void main(String[] args) {
+		// 메인메소드에서 프로그램 시작
+		// 자동정렬 ctrl+shift+f
+		System.out.println("안녕하세요");
+
+		System.out.println("새 자바 프로그램을");
+
+		System.out.println("실행 합니다.");
+	}
+
+}
