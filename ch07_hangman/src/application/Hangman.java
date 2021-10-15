4765ad2363fae66f1017e7ad6ddba81e365b9770
haun <gkdns12345@daum.net>
2021-10-15 15:53:08 +0900

    10월15일 오후수업

--- ch07_hangman/src/application/Hangman.java
+++ ch07_hangman/src/application/Hangman.java
@@ -1,9 +1,12 @@
 package application;
 
+import java.util.Scanner;
+
 public class Hangman {
 	
-	private boolean running = false;
-	
+	private boolean running = true;
+	Randomword word = new Randomword();
+	private Scanner scanner = new Scanner(System.in);
 	
 	public void run() {
 		//게임시작
@@ -12,6 +15,7 @@ public class Hangman {
 		2.철자 입력
 		3.철자가 맞는지 체크
 		*/
+		
 		do {
 			displayword(); //화면에 단어표시
 			getUserInput(); //입력받음
@@ -20,17 +24,25 @@ public class Hangman {
 		} while (running);
 	}
 	
+	
 	public void checkUserInput() {
 		System.out.println("체크");
 	}
+	
+	
 	public void getUserInput() {
-		System.out.println("단어맞추기");
+		//유저에게 한 문자 입력 요구
+		//입력받은 문자열에서 그 문자를 뽑아서 randomword 클래스에 전달 =>addguess(문자)
+		System.out.print("한 문자 입력 : ");
+		String guess = scanner.nextLine();
+		word.addGuess(guess.charAt(0));
 	}
+	
+		
 	public void displayword() {
 		//랜덤 단어 출력
 		
-		Randomword word = new Randomword();
-		System.out.println(word);
+		System.out.println(word.toString());
 	}
 
 
--- ch07_hangman/src/application/Randomword.java
+++ ch07_hangman/src/application/Randomword.java
@@ -8,25 +8,47 @@ import java.util.Random;
  */
 public class Randomword {
 	private String books = "construction direction software connection security management suggestion development obligation operation shopping revolution judgment efficiency signature teaching independence difficulty promotion investment technology bathroom measurement database president reputation platform knowledge director literature indication assistant property boyfriend advertising hospital perception analysis inspector inflation contract criticism communication performance television introduction equipment preference employment membership";
-	private String [] words = books.split(" ");
+	private String[] words = books.split(" ");
+
 	
-	private String selectword;
-	
+	private Random random = new Random();
+	private String selectword = words[random.nextInt(words.length)];// 랜덤으로 단어를 선택
+	private char[] characters; // 문자 배열 공간 생성
+
 	public Randomword() {
-		Random random = new Random();
-		int randomIndex = random.nextInt(words.length);
-		
-		selectword = words[randomIndex]; //랜덤으로 단어를 선택
+		selectword = words[random.nextInt(words.length)];
+		characters = new char[selectword.length()]; // 문자 배열 공간 생성
 	}
-	
+
 	public String toString() {
-		return selectword;
+
+//		String text = "";
+//		characters[3] = 'x';
+		
+		StringBuilder sb = new StringBuilder();
+
+		for (char c : characters) {
+			sb.append(c== '\u0000'? '_' : c);
+			sb.append(' ');
+		}
+
+		System.out.println(selectword);
+		return sb.toString();
+	}
+
+	public void getWords() {
+		for (String w : words) {
+			System.out.println(w);
+		}
 	}
 	
-	
-	public void getWords() {
-		for(String w : words) {
-			System.out.println(w);
+	public void addGuess(char c) {
+		//사용자가 입력한 한 무자를 선택된 단어에서 같은게 있는지 확인
+		//있으면 characters의 인덱스 위치에 입력
+		for(int i=0; i < selectword.length(); i++) {
+			if(c == selectword.charAt(i)) {
+				characters[i] = c;
+			}
 		}
 	}
 }
